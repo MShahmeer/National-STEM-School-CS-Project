d4254e1f58f9007acd74fd1c75c71172d674ae8f
MShahmeer <shahmeer.athar@gmail>
2018-01-04 15:41:30 +0500

    Cleaned up Main.py a bit

--- Main.py
+++ Main.py
@@ -5,7 +5,6 @@ boardArray = []
 indexLabels = {0:"top-left", 1:"top-centre", 2:"top-right", 3:"centre-left", 4:"centre", 5:"centre-right", 6:"bottom-left", 7:"bottom-centre", 8:"bottom-right"}
 gridArray = ["~", "~", "~", "~", "~", "~", "~", "~", "~"]
 compatibleCombinations = []
-solution = []
 
 def printGrid(grid):
     print  (grid[0] + "|" + grid[1]  + "|" + grid[2])
@@ -22,14 +21,12 @@ def findGoldenCube(solution):
             print("The golden cube is at the " + indexLabels[index] + " position.")
 
 def solve(boardArray):
+    solution = []
     for combination in itertools.permutations(boardArray, len(boardArray)):
-        if Rules.columnPosition(combination, "left", "green", 2):
-            if Rules.rowPosition(combination, "bottom", "teal", 2):
-                if Rules.rowPosition(combination, "top", "lightBlue", 3):
-                    if Rules.columnPosition(combination, "right", "orange", 1):
-                        for cube in combination:
-                            solution.append(cube)
-                        findGoldenCube(solution)
-                        break
+        if Rules.columnPosition(combination, "left", "green", 2) and \
+            Rules.rowPosition(combination, "bottom", "teal", 2) and \
+            Rules.rowPosition(combination, "top", "lightBlue", 3) and \
+            Rules.columnPosition(combination, "right", "orange", 1):
+                return combination
 
-solve(["lightBlue", "lightBlue", "lightBlue", "green", "green", "orange", "teal", "teal", "golden"])
+findGoldenCube(solve(["lightBlue", "lightBlue", "lightBlue", "green", "green", "orange", "teal", "teal", "golden"]))
