c3faede9acc509946a8d9db469879e0c8f46bbe5
MShahmeer <shahmeer.athar@gmail>
2018-01-04 12:39:22 +0500

    Implemented column position rule

--- Rules.py
+++ Rules.py
@@ -1,8 +1,38 @@
 #Rule for position of cube in certain column
 def columnPosition(combination, position, colour, quantity):
     if position == "left":
-        if combination[0] == colour and combination[3] == colour or combination[6] == colour:
-            print(combination)
+        leftColumnArray = [combination[0], combination[3], combination[6]]
+        matchingCubes = 0
+        for cube in leftColumnArray:
+            if cube == colour:
+                matchingCubes += 1
+            else:
+                pass
+        if matchingCubes >= quantity:
+            return True
+        else:
+            return False
+    elif position == "right":
+        rightColumnArray = [combination[2], combination[5], combination[8]]
+        matchingCubes = 0
+        for cube in rightColumnArray:
+            if cube == colour:
+                matchingCubes += 1
+            else:
+                pass
+        if matchingCubes >= quantity:
+            return True
+        else:
+            return False
+    elif position == "centre":
+        centreColumnArray = [combination[1], combination[4], combination[7]]
+        matchingCubes = 0
+        for cube in centreColumnArray:
+            if cube == colour:
+                matchingCubes += 1
+            else:
+                pass
+        if matchingCubes >= quantity:
             return True
         else:
             return False
